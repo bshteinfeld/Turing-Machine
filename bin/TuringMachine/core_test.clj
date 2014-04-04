c44011ee59447ec2d7b08a586bf8ce35fb46fdf2
Ben Shteinfeld <b.shteinfeld@gmail.com>
2014-04-04 16:24:07 -0400

    Initial Commit

---
+++ bin/TuringMachine/core_test.clj
@@ -0,0 +1,7 @@
+(ns TuringMachine.core-test
+  (:require [clojure.test :refer :all]
+            [TuringMachine.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
